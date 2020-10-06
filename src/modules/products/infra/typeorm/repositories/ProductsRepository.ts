565591fb7eff6f0f69e3d7d2a504430151545427
Carlos Wimmer <coiote777@gmail.com>
2020-10-06 20:32:50 -0300

    Create controller, repository and service of Products

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -21,21 +21,49 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = await this.ormRepository.create({ name, price, quantity });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const product = await this.ormRepository.findOne({ where: { name } });
+
+    return product;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const productsFound = await this.ormRepository.findByIds(products);
+
+    return productsFound;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const productsIds = products.map(product => product.id);
+
+    const productsUpdatable = await this.ormRepository.findByIds(productsIds);
+
+    const productsUpdated: Product[] = [];
+
+    productsUpdatable.forEach(async product => {
+      const updated: Product = { ...product };
+
+      const productSold = products.find(item => item.id === product.id);
+
+      if (productSold) {
+        updated.quantity = -productSold.quantity;
+
+        productsUpdated.push(updated);
+
+        await this.ormRepository.save(updated);
+      }
+    });
+
+    return productsUpdated;
   }
 }
 
