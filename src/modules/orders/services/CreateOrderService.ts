bb803c019c3a9fe392012b90b1f2f359445766e0
Carlos Wimmer <coiote777@gmail.com>
2020-10-07 08:06:57 -0300

    Create repository, controller and service of Orders

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -6,6 +6,7 @@ import IProductsRepository from '@modules/products/repositories/IProductsReposit
 import ICustomersRepository from '@modules/customers/repositories/ICustomersRepository';
 import Order from '../infra/typeorm/entities/Order';
 import IOrdersRepository from '../repositories/IOrdersRepository';
+import OrdersProducts from '../infra/typeorm/entities/OrdersProducts';
 
 interface IProduct {
   id: string;
@@ -20,13 +21,66 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customerFound = await this.customersRepository.findById(customer_id);
+
+    if (!customerFound) {
+      throw new AppError('Make an order using a valid customer.');
+    }
+
+    const productsFound = await this.productsRepository.findAllById(products);
+
+    if (productsFound.length < products.length) {
+      throw new AppError('There are invalid products in your list.');
+    }
+
+    const productsWithNoQuantity = productsFound.filter(product => {
+      const equivalent = products.find(item => item.id === product.id);
+
+      if (equivalent && equivalent.quantity > product.quantity) {
+        return product;
+      }
+
+      return undefined;
+    });
+
+    if (productsWithNoQuantity.length) {
+      throw new AppError(
+        `We have less ${productsWithNoQuantity[0].name} then you order.`,
+      );
+    }
+
+    const serializedProducts = products.map(product => {
+      const priceFound = productsFound.find(found => product.id === found.id)
+        ?.price as number;
+
+      const serialized = {
+        product_id: product.id,
+        price: priceFound,
+        quantity: product.quantity,
+      };
+
+      return serialized;
+    });
+
+    const order = await this.ordersRepository.create({
+      customer: customerFound,
+      products: serializedProducts,
+    });
+
+    await this.productsRepository.updateQuantity(products);
+
+    return order;
   }
 }
 
